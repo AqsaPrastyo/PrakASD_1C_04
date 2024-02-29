c6dcb4761d538219662e21561b13d406d8c6370e
Herry <aqsaherryprasetya@gmail.com>
2024-02-29 08:58:56 +0700

    P2_Percobaan2_Pertanyaan5_1C_04

--- src/P2/PenjualanBuku/BukuMain04.java
+++ src/P2/PenjualanBuku/BukuMain04.java
@@ -4,6 +4,7 @@ public class BukuMain04 {
   public static void main(String[] args) {
     
     Buku04 bk1 = new Buku04();
+    Buku04 bk2 = new Buku04();
 
     bk1.Judul = "Today Ends Tomorrow Comes";
     bk1.Pengarang = "Denanda Pratiwi";
@@ -15,5 +16,17 @@ public class BukuMain04 {
     bk1.Terjual(1);
     bk1.Gantiharga(60000);
     bk1.TampilkanInformasi();
+
+    bk2.Judul = "How to train your dragon";
+    bk2.Pengarang = "Cressida Cowell";
+    bk2.Halaman = 432;
+    bk2.Stok = 0;
+    bk2.Harga = 275000;
+
+
+    
+
+
+
   }
 }

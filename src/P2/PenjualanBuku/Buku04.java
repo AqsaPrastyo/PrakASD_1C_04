b3586e531396f5c49e31b0e29f4d6e89f6ecb01c
Herry <aqsaherryprasetya@gmail.com>
2024-02-29 08:38:23 +0700

    P2_Percobaan2_1C_04

--- src/P2/PenjualanBuku/Buku04.java
+++ src/P2/PenjualanBuku/Buku04.java
@@ -1,24 +1,23 @@
 package P2.PenjualanBuku;
 
 public class Buku04 {
-  /**
-   * InnerBuku04
-   */
+
   
   
     String Judul, Pengarang;
     int Halaman, Stok, Harga;
 
     void TampilkanInformasi (){
-      System.out.println("Judul " + Judul);
-      System.out.println("Pengarang " + Pengarang);
-      System.out.println("Jumlah halaman " + Halaman);
-      System.out.println("Sisa stok " + Stok);
-      System.out.println("Harga Rp " + Harga);
+      System.out.println("Judul: " + Judul);
+      System.out.println("Pengarang: " + Pengarang);
+      System.out.println("Jumlah halaman: " + Halaman);
+      System.out.println("Sisa stok: " + Stok);
+      System.out.println("Harga Rp. " + Harga);
     }
 
     void Terjual(int jml){
-      Stok -= jml;
+      Stok -= jml;  
+      
     }
 
     void restock(int jml){
---
+++ src/P2/PenjualanBuku/BukuMain04.java
@@ -0,0 +1,19 @@
+package P2.PenjualanBuku;
+
+public class BukuMain04 {
+  public static void main(String[] args) {
+    
+    Buku04 bk1 = new Buku04();
+
+    bk1.Judul = "Today Ends Tomorrow Comes";
+    bk1.Pengarang = "Denanda Pratiwi";
+    bk1.Halaman = 198;
+    bk1.Stok = 13;
+    bk1.Harga = 71000;
+
+    bk1.TampilkanInformasi();
+    bk1.Terjual(5);
+    bk1.Gantiharga(60000);
+    bk1.TampilkanInformasi();
+  }
+}

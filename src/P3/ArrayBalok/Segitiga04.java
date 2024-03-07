c86a961ceaee397a52d80074292861daaa299f28
Herry <aqsaherryprasetya@gmail.com>
2024-03-07 11:21:04 +0700

    P3_Pertanyaan2_Percobaan3_1C_04

---
+++ src/P3/ArrayBalok/Segitiga04.java
@@ -0,0 +1,20 @@
+package P3.ArrayBalok;
+
+public class Segitiga04 {
+  public int tinggi;
+  public int alas;
+
+
+  public Segitiga04(int a, int t){
+
+    alas = a;
+    tinggi = t;
+
+
+  }
+
+
+
+
+
+}

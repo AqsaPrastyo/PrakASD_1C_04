4a4b920a0ea3779eed23169a965d12984794730c
Herry <aqsaherryprasetya@gmail.com>
2024-03-07 11:42:14 +0700

    P3_Pertanyaan5_Percobaan3_1C_04

--- src/P3/ArrayBalok/sgArray04.java
+++ src/P3/ArrayBalok/sgArray04.java
@@ -10,7 +10,16 @@ Segitiga04[] sgArray04 = new Segitiga04[4];
     sgArray04[2] = new Segitiga04(15, 6);
     sgArray04[3] = new Segitiga04(25, 10);
 
+    for (int i = 0; i < sgArray04.length; i++) {
+      System.out.println("Segitiga ke-" + (i + 1 ) );
+      System.out.println("Alas: " + sgArray04[i].alas);
+      System.out.println("Tinggi: " + sgArray04[i].tinggi);
 
+      sgArray04[i].hitungLuas();
+      sgArray04[i].hitungKeliling();
+
+      System.out.println(); 
+  }
 
 
 

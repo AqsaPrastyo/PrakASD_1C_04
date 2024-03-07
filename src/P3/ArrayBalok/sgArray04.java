a291d85c1166c42e40360b9037941970742d24e4
Herry <aqsaherryprasetya@gmail.com>
2024-03-07 11:35:15 +0700

    P3_Pertanyaan4_Percobaan3_1C_04

---
+++ src/P3/ArrayBalok/sgArray04.java
@@ -0,0 +1,18 @@
+package P3.ArrayBalok;
+
+public class sgArray04 {
+  public static void main(String[] args) {
+    
+Segitiga04[] sgArray04 = new Segitiga04[4];
+
+    sgArray04[0] = new Segitiga04(10, 4);
+    sgArray04[1] = new Segitiga04(20, 10);
+    sgArray04[2] = new Segitiga04(15, 6);
+    sgArray04[3] = new Segitiga04(25, 10);
+
+
+
+
+
+  }
+}

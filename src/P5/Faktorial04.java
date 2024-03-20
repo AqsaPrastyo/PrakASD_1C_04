e6833a811fcbcf14938dd7e3a80661263138548c
Herry <aqsaherryprasetya@gmail.com>
2024-03-20 23:55:02 +0700

    P5_Percobaan1_Pertanyaan2_1C_04

--- src/P5/Faktorial04.java
+++ src/P5/Faktorial04.java
@@ -5,26 +5,39 @@ public class Faktorial04 {
 public int nilai;
 
 
-int faktorialBF(int n){
-  int fakto = 1;
-  for (int i = 1; i <= n; i++) {
-    fakto = fakto *= i;
-  }
-  return fakto;
-}
+// int faktorialBF(int n){
+//   int fakto = 1;
+//   for (int i = 1; i <= n; i++) {
+//     fakto = fakto *= i;
+//   }
+//   return fakto;
+// }
 
-int faktoriaDC(int n){
-  if (n == 1) {
+// int faktoriaDC(int n){
+//   if (n == 1) {
+//       return 1;
+//   }else{
+//     int fakto = n * faktoriaDC(n - 1);
+//     return fakto;
+//   }
+// }
+
+int faktorialBF(int n) {
+  if (n == 0 || n == 1) {
       return 1;
-  }else{
-    int fakto = n * faktoriaDC(n - 1);
-    return fakto;
+  } else {
+      return n * faktorialBF(n - 1);
   }
 }
 
-
-
-
+int faktoriaDC(int n) {
+        if (n == 1) {
+            return 1;
+        } else {
+            int fakto = n * faktoriaDC(n - 1);
+            return fakto;
+        }
+    }
 
 
 

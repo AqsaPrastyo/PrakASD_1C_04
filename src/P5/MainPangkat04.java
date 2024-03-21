0db95c15d67e416c2be8867df9bbcb18cb2eaf6a
Herry <aqsaherryprasetya@gmail.com>
2024-03-21 07:55:21 +0700

    P5_Percobaan2_1C_04

---
+++ src/P5/MainPangkat04.java
@@ -0,0 +1,39 @@
+package P5;
+
+import java.util.Scanner;
+
+public class MainPangkat04 {
+    public static void main(String[] args) {
+        Scanner sc04 = new Scanner(System.in);
+
+        System.out.println("----------------------");
+        System.out.print("Masukan jumlah elemen yang dihitung: ");
+        int iLmn = sc04.nextInt();
+
+        Pangkat04[] png = new Pangkat04[iLmn];
+        for (int i = 0; i < png.length; i++) {
+            png[i] = new Pangkat04();
+            System.out.println("Masukan nilai yang hendak dipangkatkan: ");
+            png[i].nilai = sc04.nextInt(); // Simpan nilai yang dibaca ke dalam objek
+            System.out.println("Masukan nilai pemangkat: ");
+            png[i].pangkat = sc04.nextInt(); // Simpan pangkat yang dibaca ke dalam objek
+        }
+
+        System.out.println("HASIL PANGKAT - BRUTE FORCE");
+        for (int i = 0; i < iLmn; i++) {
+            System.out.println(
+                    "Hasil dari " + png[i].nilai + " pangkat " + png[i].pangkat + " adalah " +
+                            png[i].pangkatBF(png[i].nilai, png[i].pangkat)
+            );
+        }
+
+        System.out.println("HASIL PANGKAT - DIVIDE CONQUER");
+        for (int i = 0; i < iLmn; i++) {
+            System.out.println(
+                    "Hasil dari " + png[i].nilai + " pangkat " + png[i].pangkat + " adalah " +
+                            png[i].pangkatDC(png[i].nilai, png[i].pangkat)
+            );
+        }
+    }
+}
+
---
+++ src/P5/Pangkat04.java
@@ -0,0 +1,31 @@
+package P5;
+
+public class Pangkat04 {
+  public int nilai, pangkat;
+
+
+int pangkatBF (int a, int n){
+int hasil = 1;
+for (int i = 0; i < n; i++) {
+  hasil *= a;
+}
+return hasil;
+}
+
+int pangkatDC (int a, int n){
+  if (n==0) {
+    return 1;
+  }else{
+      int nilai = pangkatDC(a, n/2);
+      if (n%2==1) {
+        return nilai * nilai * a;
+      } else {
+        return nilai * nilai;
+      }
+
+
+
+  }
+}
+}
+

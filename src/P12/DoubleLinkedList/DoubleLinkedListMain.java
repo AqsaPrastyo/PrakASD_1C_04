0846a85c7b62ed9cfed63105477d58bb46c03051
Herry <aqsaherryprasetya@gmail.com>
2024-05-21 15:27:26 +0700

    P12_Pecobaan3_1C_04

--- src/P12/DoubleLinkedList/DoubleLinkedListMain.java
+++ src/P12/DoubleLinkedList/DoubleLinkedListMain.java
@@ -21,24 +21,41 @@ public class DoubleLinkedListMain {
     // System.out.println("Size : "+dll.size());
     // System.out.println("=========================================");
 
-    dll.addLast(50);
-    dll.addLast(40);
-    dll.addLast(10);
-    dll.addLast(20);
+    // dll.addLast(50);
+    // dll.addLast(40);
+    // dll.addLast(10);
+    // dll.addLast(20);
+    // dll.print();
+    // System.out.println("Size : "+dll.size());
+    // System.out.println("=========================================");
+    // dll.removeFirst();
+    // dll.print();
+    // System.out.println("Size : "+dll.size());
+    // System.out.println("=========================================");
+    // dll.removeLast();
+    // dll.print();
+    // System.out.println("Size : "+dll.size());
+    // System.out.println("=========================================");
+    // dll.remove(1);
+    // dll.print();
+    // System.out.println("Size : "+dll.size());
+    // System.out.println("=========================================");
+
     dll.print();
     System.out.println("Size : "+dll.size());
     System.out.println("=========================================");
-    dll.removeFirst();
+    dll.addFirst(3);
+    dll.addLast(4);
+    dll.addFirst(7);
     dll.print();
     System.out.println("Size : "+dll.size());
     System.out.println("=========================================");
-    dll.removeLast();
+    dll.add(40, 1);
     dll.print();
     System.out.println("Size : "+dll.size());
     System.out.println("=========================================");
-    dll.remove(1);
-    dll.print();
-    System.out.println("Size : "+dll.size());
-    System.out.println("=========================================");
+    System.out.println("Data awal pada linked list adalah " + dll.getFirst());
+    System.out.println("Data akhir pada linked list adalha " + dll.getLast());
+    System.out.println("Data indeks ke-1 pada linked list adalah: " + dll.get(1));
   }
 }

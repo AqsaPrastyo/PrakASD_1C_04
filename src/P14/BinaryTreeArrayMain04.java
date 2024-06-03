b898001094d6a23c010f1e0db60bf66434109ca2
Herry <aqsaherryprasetya@gmail.com>
2024-06-03 12:12:34 +0700

    P14_Tugas2_1C_04

--- src/P14/BinaryTreeArrayMain04.java
+++ src/P14/BinaryTreeArrayMain04.java
@@ -1,13 +1,38 @@
 package P14;
 
 public class BinaryTreeArrayMain04 {
- public static void main(String[] args) {
-  BinaryTreeArray04 bta = new BinaryTreeArray04();
-  int [] data = {6,4,8,3,5,7,9,0,0,0};
-  int idxLast =6;
-  bta.populateData(data, idxLast);
-  System.out.println("\nInOrder Tranversal : ");
-  bta.traverseInOrder(0);
-  System.out.println("\n");
- } 
+    public static void main(String[] args) {
+        BinaryTreeArray04 bta = new BinaryTreeArray04();
+        int[] data = {6, 4, 8, 3, 5, 7, 9, 0, 0, 0};
+        int idxLast = 6;
+        bta.populateData(data, idxLast);
+
+        System.out.println("InOrder Traversal:");
+        bta.traverseInOrder(0);
+        System.out.println();
+
+        System.out.println("PreOrder Traversal:");
+        bta.traversePreOrder(0);
+        System.out.println();
+
+        System.out.println("PostOrder Traversal:");
+        bta.traversePostOrder(0);
+        System.out.println();
+
+        
+        bta.add(10);
+        bta.add(11);
+
+        System.out.println("\nInOrder Traversal setelah menambahkan elements:");
+        bta.traverseInOrder(0);
+        System.out.println();
+
+        System.out.println("PreOrder Traversal setelah menambahkan elements:");
+        bta.traversePreOrder(0);
+        System.out.println();
+
+        System.out.println("PostOrder Traversal setelah menambahkan elements:");
+        bta.traversePostOrder(0);
+        System.out.println();
+    }
 }

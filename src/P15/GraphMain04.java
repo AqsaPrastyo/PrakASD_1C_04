a088bff840e8a6ae60afda18de8c6d03c16a353f
Herry <aqsaherryprasetya@gmail.com>
2024-06-06 11:20:39 +0700

    P15_Percobaan2_1C_04

--- src/P15/GraphMain04.java
+++ src/P15/GraphMain04.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class GraphMain04 {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
+    GraphMatrik04 gdg = new GraphMatrik04(4);
     Graph04 gedung  = new Graph04(6);
     gedung.addEdge(0, 1, 50, true);
     gedung.addEdge(0, 2, 100, true);
@@ -48,6 +49,25 @@ public class GraphMain04 {
     } else {
       System.out.println("Gedung " + (char)('A' + asal) + " dan Gedung " + (char)('A' + tujuan) + " tidak bertetangga");
     }
+
+gdg.makeedge(0, 1, 50);
+gdg.makeedge(1, 0, 60);
+gdg.makeedge(1, 2, 70);
+gdg.makeedge(2, 1, 80);
+gdg.makeedge(2, 3, 40);
+gdg.makeedge(3, 0, 90);
+try {
+  gdg.printGraph();
+} catch (Exception e) {
+  e.printStackTrace();
+}
+System.out.println("Hasil setelah penghapusan edge");
+gdg.removeEdge(2, 1);
+try {
+  gdg.printGraph();
+} catch (Exception e) {
+  e.printStackTrace();
+}
 
     scanner.close();
   }
---
+++ src/P15/GraphMatrik04.java
@@ -0,0 +1,36 @@
+package P15;
+
+public class GraphMatrik04 {
+  int vertex;
+  int [][] matriks;
+  public GraphMatrik04(int v){
+    vertex = v;
+    matriks = new int[v][v];
+  }
+
+  public  void makeedge(int asal, int tujuan, int jarak){
+    matriks[asal][tujuan] = jarak;
+  }
+  public void removeEdge(int asal, int tujuan){
+    matriks[asal][tujuan] = -1;
+  } 
+
+  public void printGraph() throws Exception {
+    for (int i = 0; i < vertex; i++) {
+        System.out.print("Gedung " + (char)('A' + i) + ": ");
+        for (int j = 0; j < vertex; j++) {
+          if (matriks[i][j] != -1) {
+            System.out.print("Gedung " + (char)('A' + j) + " (" + matriks[i][j] + " m), ");
+          }
+        }
+        System.out.println();
+    }
+  }
+
+
+
+
+
+
+
+}
